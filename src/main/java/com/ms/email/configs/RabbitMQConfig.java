d900799daf83fe7f194656ecc5ba2f2dd1ee817c
RafaelHen <rafaelvictor368@hotmail.com>
2024-03-31 23:47:26 -0300

    Criacao de calsses para escutar filas e converter mensagens em objetos

--- src/main/java/com/ms/email/configs/RabbitMQConfig.java
+++ src/main/java/com/ms/email/configs/RabbitMQConfig.java
@@ -1,6 +1,7 @@
 package com.ms.email.configs;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,12 @@ public class RabbitMQConfig {
     private String queue;
 
     @Bean
-    public Queue queue() {
+    Queue queue() {
         return new Queue(queue, true);
+    }
+    
+    @Bean
+    Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
